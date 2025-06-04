683ea787c4193c1e05114c4ba928cb7f9cd88f1c
alisherios <125376347+alisherios@users.noreply.github.com>
2025-06-04 10:20:54 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-# Запуск приложения
+#!/usr/bin/env python3
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
